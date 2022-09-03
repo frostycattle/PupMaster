79f0e4591efa2ef43f9d8eafa9b87f87e51159fd
frostycattle <110635916+frostycattle@users.noreply.github.com>
2022-09-02 22:26:39 -0400

    Update pup.py

--- pup.py
+++ pup.py
@@ -1,4 +1,51 @@
-import argparser
 import requests
-import os
-import time
+import pyfiglet
+import sys
+
+print("-" * 58)
+print(pyfiglet.figlet_format("PAGE FINDER"))
+print("-" * 62)
+
+url = input("URL: ")
+
+try:
+    requests.get(url)
+
+except Exception as ex:
+    print(ex)
+    sys.exit(1)
+
+wordlistLocation = input("Wordlist: ")
+
+try:
+    wordlist = open(wordlistLocation, "r")
+    words = []
+
+    for line in wordlist:
+        words.append(line[:-1])
+        print(words)
+
+
+    permision = input("Start y/n: ")
+
+    if permision.__eq__(" y") or permision.__eq__("y"):
+        print("Starting")
+
+        try:
+            for element in words:
+                request = requests.get(url + "/" + element)
+
+                code = request.status_code.__str__()
+
+                print(url + "/" + element + " " + request.status_code.__str__())
+
+
+        except Exception as ex:
+            print(ex)
+
+    else:
+        print("Exiting...")
+
+
+except Exception as ex:
+    print(ex)
