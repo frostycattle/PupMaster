2c37ae372b6254e0e8bc847e7e1c08a5faaead8b
frostycattle <110635916+frostycattle@users.noreply.github.com>
2022-10-20 06:34:13 -0400

    Update pup.py

--- pup.py
+++ pup.py
@@ -1,5 +1,5 @@
 
-# Remove output file arguments, and associations.
+# the defination of shit is here
 
 from threading import Thread
 from os.path import exists
@@ -9,7 +9,7 @@ import sys
 import requests
 import os
 
-banner = "---------------------------------------"
+banner = "------------------------------------------"
 
 print(banner)
 
@@ -19,6 +19,7 @@ print("██████╔╝██║   ██║██████╔╝█�
 print("██╔═══╝ ██║   ██║██╔═══╝ ██╔═══╝   ╚██╔╝ " )
 print("██║     ╚██████╔╝██║██╗  ██║        ██║ "  )
 print("╚═╝      ╚═════╝ ╚═╝╚═╝  ╚═╝        ╚═╝"  )
+
 
 
 # The default keyword for the program. You can change this to a custom one using the argument --keyword.
@@ -36,9 +37,7 @@ parser = argparse.ArgumentParser(description="Use this tool to discover content 
 parser.add_argument("-w", help="REQUIRED: Input the wordlist location here.")
 parser.add_argument("-d", help="REQUIRED: Input the (URL) here.")
 parser.add_argument("--filter", help="OPTIONAL: Filter the response(s) by response code.", type=int)
-parser.add_argument("--filters", help="WORK IN PROGRESS: Specify a list of codes to filter the response(s) by.")
 parser.add_argument("--algorithm", help="OPTIONAL: Use a built-in algorithm to send requests faster.")
-parser.add_argument("--cloudwordlist", help="WORK IN PROGRESS: Link a wordlist in the cloud.")
 parser.add_argument("--keyword", help="OPTIONAL: Specfiy your own 'FUZZ' keyword.")
 parser.add_argument("--output", help="OPTIONAL: Link an output file.")
 
@@ -46,153 +45,98 @@ arguments = parser.parse_args()
 
 # Variables store the values provided by the arguments
 
-wordlistLocation = arguments.w
 domain = arguments.d
-responseCodeToFilter = arguments.filter
-#wordlistsList = arguments.ws
 
-# The cloudwordlist argument is currently broken. Do not use it.
+print(banner)
 
-cloudwordlistLocation = arguments.cloudwordlist
-keyword = arguments.keyword
-codeList = arguments.filters
-output = arguments.output
+def returnCode(code):
+	return 
 
+def sendRequest(url):
 
-def convertFiltersToList(string):
+	try:
+		r = requests.get("http://" + url)
 
-	if codeList == None:
-		return
+		return r.status_code
 
-	else:
-
-		filters = list(string.split(" "))
-		return filters
-
-
-responseList = convertFiltersToList(codeList)
-
-# Print a space
-
-print("")
-
-try:
-
-	print(banner)
-
-	# Check to see if an custom "FUZZ" keyword was provided.
-
-	if keyword == None:
-		keyword = defaultKeyword
-
-	else:
+	except Exception as ex:
 		pass
 
-	# Check if "FUZZ" is defined in the given url
-
-	if keyword in domain:
-		pass
-
-	else:
-		print("ERROR: Could not find " + keyword + " in " + domain + "!")
-		sys.exit(0)
-
-	if output == None:
-		pass
+def checkForKeywordInDomain(domain):
+	if defaultKeyword in domain:
+		return True
 
 	else:
-		try:
-			if exists(output):
-				print("Output file: " + output)
+		return False
+
+class CheckForKeyWordInDomain:
+	def __init__(self):
+		pass
+
+	def checkForKeyWordInDomain(self, domain):
+		if defaultKeyword in domain:
+			return True 
+
+		else:
+			return False
+
+class Enumerate:
+	def __init__(self, line, domain, filter, outputFileLoc):
+
+			if outputFileLoc == None:
 				pass
 
-
 			else:
-				createFile = input("Could not find the output file. Create one? ")
+				output = open(outputFileLoc, "a")
 
-				if createFile.__eq__("yes") or createFile.__eq__(" yes") or createFile.__eq__("y") or createFile.__eq__(" y"):
-					try:
-						outputFile = open(output, "x")
-						outputFile.close()
 
-					except Exception as ex:
-						print("ERROR: Error while creating file! " + ex)
-						sys.exit(0)
+			url = domain.replace(defaultKeyword, line)
 
-		except Exception as ex:
-			print(ex)
-			sys.exit(0)
+			code = sendRequest(url=url)
 
-	# Check if the wordlist is in the cloud or installed on the machine.
+			if filter == None:								
+				print("TRIED: " + line + ": " + str(code))
 
-	if wordlistLocation == None and cloudwordlistLocation == None:
-		print("ERROR: Wordlist location not specifed!")
-		sys.exit(0)
+			elif filter <= 299:
+				if code == filter:
+					print("FOUND: " + line)	
 
-	else:
-		if wordlistLocation == None:
-			
-			try:
-				cloudwordlistRequest = requests.get(cloudwordlistLocation)
-				cloudwordlist = cloudwordlistRequest.text
-				print(cloudwordlist)
+					if outputFileLoc == None:
+						pass
 
-			except Exception as ex:
-				print(ex)
+					else:
+						output.write(str(url + "\n"))
+					
 
-			print("Cloudwordlist: " + cloudwordlistLocation)
+class ParseWordlist:
+	def __init__(self, wordlistLoc, outputLoc):
 
-		else:
-			print("Wordlist: " + wordlistLocation)
-	
-	print("Domain: " + domain)
+		wordlist = open(wordlistLoc, "r")
 
-		# Try to open the output file
-	if responseCodeToFilter == None:
-		pass
+		for line in wordlist:
+			enumerate = Enumerate(line[:-1], arguments.d, arguments.filter, outputLoc)
+
+
+
+# Check if output file is provided
+
+if arguments.output == None:
+	pass
+
+	if CheckForKeyWordInDomain().checkForKeyWordInDomain(arguments.d) == True:
+		parseWordlist = ParseWordlist(arguments.w, None)
 
 	else:
-		print("Filtering by code: "+str(responseCodeToFilter))
+		print("ERROR: Keyword not found in domain.")
 
 
-except Exception as ex:
-	print("ERROR: "+str(ex))
-	print(exitBanner)
-	sys.exit(0)
+else:
+
+	if CheckForKeyWordInDomain().checkForKeyWordInDomain(arguments.d) == True:
+		parseWordlist = ParseWordlist(arguments.w, arguments.output)
+
+	else:
+		print("ERROR: Keyword not found in domain.")
 
 
-# Begin enumurateing
-
-print(banner)
-
-wordlist =  open(wordlistLocation, "r")
-for line in wordlist:
-
-		url = domain.replace(keyword, line[:-1])
-
-		try:
-
-			request = requests.get("http://"+url)
-		
-		except Exception as ex:
-			print("ERROR: " + str(ex))
-
-		if responseCodeToFilter == None:
-			print(url + " : " + str(request.status_code))
-
-		else:
-			pass
-
-		if request.status_code == responseCodeToFilter < 299:
-			print("FOUND: " +line[:-1])
-
-
-		elif request.status_code == responseCodeToFilter:
-			print("/"+line[:-1] + " : " + str(request.status_code))
-
-			
-		else:
-			pass
-
-
-print(banner)
+# Check if the keyword is in the domain, and start if true
